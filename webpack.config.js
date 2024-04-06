5433541add2c470dc784f47b4d32343cbf5a8bac
akami0000 <akaikinomi0325@gmail.com>
2024-04-06 23:32:17 +0900

    OGPの設定

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
         generator: {
-          filename: `./image/[name].[contenthash][ext]`
+          filename: `./image/[name][ext]`
         },
         type: 'asset/resource'
       },
